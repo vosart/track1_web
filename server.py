8bb9a264b175db8361f4636b04877ce033acef26
Artem Voskresensky <metravs@gmail.com>
2022-12-02 02:28:30 +0300

    weather API

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from weather import weather_by_city
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    weather = weather_by_city("Rostov-on-Don,Russia")
+    if weather:
+        return f"Температура: {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
+    else:
+        return "Сервис погоды временно недоступен"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
